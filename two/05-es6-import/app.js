0e236b6cb5f48eb2861b27b32819068f5e4bffab
Big-stone <597685607@qq.com>
2017-04-20 23:03:13 +0800

    es6-import导入模块

---
+++ two/05-es6-import/app.js
@@ -0,0 +1,7 @@
+//es6中导入模块语法
+
+// 导入模块
+
+import math from './math'
+
+math.add();
---
+++ two/05-es6-import/math.js
@@ -0,0 +1,30 @@
+
+// commonjs规范的模块化语法
+
+// module.exports = {
+//     add: function(){
+
+//     }
+// }
+
+// es6的模块化语法
+
+// 导出模块
+
+// export default {
+//     add: function() {
+//         console.log('add测试');
+//     },
+//     sub: function() {
+
+//     }
+// }
+
+export default {
+    add: function() {
+        console.log('add测试');
+    },
+    sub: function() {
+
+    }
+}
