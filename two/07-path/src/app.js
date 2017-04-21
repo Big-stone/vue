86b4e6add50d0c683a4484559f0877309275f60f
Big-stone <597685607@qq.com>
2017-04-21 18:39:28 +0800

    热加载webpack-dev-server

---
+++ two/07-path/src/app.js
@@ -0,0 +1,9 @@
+import math from './math' //导入math
+
+math.add();
+
+let  a = 1;
+console.log(a);
+
+let b = 2;
+console.log(b);
---
+++ two/07-path/src/math.js
@@ -0,0 +1,9 @@
+
+//导出模块
+export default {
+    add() {
+        console.log('hello');
+
+        console.log('world');
+    }
+}
